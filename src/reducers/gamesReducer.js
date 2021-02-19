63b13062e075c31e76091778c9bf79f50e0d4fb4
Kovacs Sandor <sada93@citromail.hu>
2021-02-19 16:00:17 +0100

    6. Fetching Other Games

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,15 +1,20 @@
 
 const initState = {
     popular: [],
-    newGames: [],
-    upComing: [],
+    new: [],
+    upcoming: [],
     searched: []
 }
 
 const gameReducer = (state = initState, action) => {
     switch (action.type) {
         case "FETCH_GAMES":
-            return { ...state, popular: action.payload.popular };
+            return {
+                ...state,
+                popular: action.payload.popular,
+                upcoming: action.payload.upcoming,
+                new: action.payload.new,
+            };
         default:
             return { ...state };
     }
